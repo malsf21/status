d2bfcca167bf9800bd0cfd319e6e71b62f9071b3
Matthew Wang <matthewang.wang@gmail.com>
2016-03-17 10:53:56 -0400

    radical bugtesting

--- js/league_parser.js
+++ js/league_parser.js
@@ -1,3 +1,43 @@
+$.ajax({
+    url: 'php/game.php?summonerid=65443758',
+    dataType: "json",
+    success: function(league_game_jsonData) {
+      console.log("true");
+      var playingLeague = true;
+      var league_game_mode = "Custom/Unidentified";
+      league_game_type = league_game_jsonData["gameQueueConfigId"];
+      if (league_game_type == 2){
+        league_game_mode = "Blind Pick";
+      }
+      else if (league_game_type == 14){
+        league_game_mode = "Normal Draft";
+      }
+      else if (league_game_type == 4){
+        league_game_mode = "Ranked";
+      }
+      else if (league_game_type == 41){
+        league_game_mode = "Ranked 3v3";
+      }
+      else if (league_game_type == 65){
+        league_game_mode = "ARAM";
+      }
+      else if (league_game_type == 31 || league_game_type == 32 || league_game_type == 33){
+        league_game_mode = "Bot";
+      }
+      for (i = 0; i < jsonData["participants"].length; i++){
+        if (jsonData['participants'][i]["summonerId"] == 65443758){
+          var champid = jsonData['participants'][i]['championId'];
+        }
+      }
+      var league_champ = champ_jsonData["data"][String(champid)]["name"];
+    },
+    error: function(data) {
+        var playingLeague = false;
+        console.log("false");
+    }
+})
+
+/*
 var league_game_data = httpGet("php/game.php?summonerid=65443758");
 var league_game_jsonData = JSON.parse(league_game_data);
 console.log(league_game_jsonData);
@@ -34,3 +74,4 @@ if (league_champ_jsonData["gameId"] != null){
   }
   var league_champ = champ_jsonData["data"][String(champid)]["name"];
 }
+*/
