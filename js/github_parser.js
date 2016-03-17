07e59d085feee0296d21e12febdfbc834f7312d5
Matthew Wang <matthewang.wang@gmail.com>
2016-03-17 09:21:51 -0400

    tests out a way to sort through github event types

--- js/github_parser.js
+++ js/github_parser.js
@@ -1,4 +1,15 @@
 var github_data = JSON.parse(httpGet("https://api.github.com/users/malsf21/events/public?jsoncallback=?"));
-var github_repo = github_data[0]["repo"]["name"].substring(8);
-var github_unix_time = new Date(Date.parse(github_data[0]["created_at"]));
+var found = false;
+var counter = 0;
+while (found == false){
+  if (github_data[counter]["type"] == "PushEvent"){
+    found = true;
+    break;
+  }
+  else{
+    counter += 1;
+  }
+}
+var github_repo = github_data[counter]["repo"]["name"].substring(8);
+var github_unix_time = new Date(Date.parse(github_data[counter]["created_at"]));
 var github_time = "at " + String(github_unix_time).substring(16,21) + " " + github_unix_time.toDateString();
