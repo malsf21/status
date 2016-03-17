f1f9c5b86df1b0b2076adbec65480adb8c829644
malsf21 <matthewang.wang@gmail.com>
2016-03-17 05:00:00 -0400

    adds twitch module

--- js/twitch_parser.js
+++ js/twitch_parser.js
@@ -1,6 +1,6 @@
 var streaming = false;
 
-var stream_data = JSON.parse(httpGet("https://api.twitch.tv/kraken/streams/malsf21?jsoncallback=?"));
+var stream_data = JSON.parse(httpGet("https://api.twitch.tv/kraken/streams/riotgames?jsoncallback=?"));
 
 if (stream_data["stream"] != null){
   streaming = true;
