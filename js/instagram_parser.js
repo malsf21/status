3a046cb991ee30750772f9034f801258075c8bb0
malsf21 <matthewang.wang@gmail.com>
2016-03-17 06:10:51 -0400

    fixes indexing error

--- js/instagram_parser.js
+++ js/instagram_parser.js
@@ -3,10 +3,10 @@ $.ajax({
     dataType : "jsonp",
     cache: false,
     success: function(instagram_data){
-      var instagram_photo = instagram_data["items"]["images"]["thumbnail"]["url"];
-      var instagram_caption = instagram_data["items"]["caption"]["text"];
-      var instagram_date = instagram_data["items"]["caption"]["created_time"];
-      var instagram_location = instagram_data["items"]["location"]["text"];
+      var instagram_photo = instagram_data["items"][0]["images"]["thumbnail"]["url"];
+      var instagram_caption = instagram_data["items"][0]["caption"]["text"];
+      var instagram_date = instagram_data["items"][0]["caption"]["created_time"];
+      var instagram_location = instagram_data["items"][0]["location"]["text"];
 
       console.log(instagram_photo);
     }
