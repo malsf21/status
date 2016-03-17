92889e2572cd1bd5bf5efe4ad2adbbec97cefc6c
malsf21 <matthewang.wang@gmail.com>
2016-03-17 06:09:20 -0400

    tries using a different method

--- js/instagram_parser.js
+++ js/instagram_parser.js
@@ -1,8 +1,13 @@
-var instagram_data = JSON.parse(httpGet("http://whateverorigin.org/get?url=" + encodeURIComponent("http://instagram.com/nonotthatmatthewwang/media")));
+$.ajax({
+    url: "http://whateverorigin.org/get?url=" + encodeURIComponent("http://instagram.com/nonotthatmatthewwang/media"),
+    dataType : "jsonp",
+    cache: false,
+    success: function(instagram_data){
+      var instagram_photo = instagram_data["items"]["images"]["thumbnail"]["url"];
+      var instagram_caption = instagram_data["items"]["caption"]["text"];
+      var instagram_date = instagram_data["items"]["caption"]["created_time"];
+      var instagram_location = instagram_data["items"]["location"]["text"];
 
-var instagram_photo = instagram_data["items"]["images"]["thumbnail"]["url"];
-var instagram_caption = instagram_data["items"]["caption"]["text"];
-var instagram_date = instagram_data["items"]["caption"]["created_time"];
-var instagram_location = instagram_data["items"]["location"]["text"];
-
-console.log(instagram_photo);
+      console.log(instagram_photo);
+    }
+});
