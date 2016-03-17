53871c1c26f9e8a92b1aedd7763e38d58255adec
Matthew Wang <matthewang.wang@gmail.com>
2016-03-17 10:34:14 -0400

    minor bugfixes

--- php_template/summoner.php
+++ php_template/summoner.php
@@ -2,7 +2,7 @@
   $log_json = file_get_contents("summoner_log.json");
   $log_data = json_decode($log_json, true);
   $current_time = time();
-  if ($current_time - $log_data["time"] >= 10){
+  if ($current_time - $log_data["time"] >= 30){
     $apikey = "";
     $url = "https://na.api.pvp.net/api/lol/na/v2.5/league/by-summoner/65443758/entry?api_key=".$apikey;
     $ch = curl_init($url);
@@ -16,7 +16,7 @@
       "time" => $current_time,
       "data" => $data
     ];
-    file_put_contents('game_log.json', json_encode($new_data, JSON_FORCE_OBJECT));
+    file_put_contents('summoner_log.json', json_encode($new_data, JSON_FORCE_OBJECT));
   }
   else{
     print(json_encode($log_data["data"], JSON_FORCE_OBJECT));
