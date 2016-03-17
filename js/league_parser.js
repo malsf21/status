307b913ee80ca0f823c0b64813450e83828cbc81
malsf21 <matthewang.wang@gmail.com>
2016-03-17 04:35:29 -0400

    tightens up naming conventions

---
+++ js/league_parser.js
@@ -0,0 +1,39 @@
+var league_game_data = httpGet("php/game_json.php?summonerid=65443758");
+var league_game_jsonData = JSON.parse(league_game_data);
+
+var playingLeague = false;
+if ("status" in league_game_jsonData){
+  playingLeague = false;
+}
+else{
+  var league_champ_data = httpGet("php/champ_json.php");
+  var league_champ_jsonData = JSON.parse(champ_data);
+
+  playingLeague = true;
+  var league_game_mode = "Custom/Unidentified";
+  league_game_type = league_game_jsonData["gameQueueConfigId"];
+  if (league_game_type == 2){
+    league_game_mode = "Blind Pick";
+  }
+  else if (league_game_type == 14){
+    league_game_mode = "Normal Draft";
+  }
+  else if (league_game_type == 4){
+    league_game_mode = "Ranked";
+  }
+  else if (league_game_type == 41){
+    league_game_mode = "Ranked 3v3";
+  }
+  else if (league_game_type == 65){
+    league_game_mode = "ARAM";
+  }
+  else if (league_game_type == 31 || league_game_type == 32 || league_game_type == 33){
+    league_game_mode = "Bot";
+  }
+  for (i = 0; i < jsonData["participants"].length; i++){
+    if (jsonData['participants'][i]["summonerId"] == 65443758){
+      var champid = jsonData['participants'][i]['championId'];
+    }
+  }
+  var league_champ = champ_jsonData["data"][String(champid)]["name"];
+}
