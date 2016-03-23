fc3667c425766127b3d0dc408b026a02c13dc0a4
Matthew Wang <matthewang.wang@gmail.com>
2016-03-23 01:15:31 -0400

    testing one bugfixing method

--- js/steam_parser.js
+++ js/steam_parser.js
@@ -6,7 +6,7 @@ var steam_state = steam_data["response"]["players"][0]["personastate"];
 var steam_status = steam_states[steam_state];
 var steam_playing = 0;
 if (steam_state == 0){
-  var steam_logoff_unix = new Date(Date.parse(Number(steam_data["response"]["players"][0]["lastlogoff"])));
+  var steam_logoff_unix = new Date(Date.parse(Number(steam_data["response"]["players"][0]["lastlogoff"]+"000")));
   var steam_logoff = "at " + String(steam_logoff_unix).substring(16,21) + " " + steam_logoff_unix.toDateString();
   console.log(steam_data["response"]["players"][0]["lastlogoff"])
   console.log(steam_logoff_unix);
