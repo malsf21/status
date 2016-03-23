f1241f91f7b27aaa8b8c3507f11b18acfdabcafe
Matthew Wang <matthewang.wang@gmail.com>
2016-03-23 01:22:05 -0400

    cleans everything up

--- js/steam_parser.js
+++ js/steam_parser.js
@@ -8,12 +8,8 @@ var steam_playing = 0;
 if (steam_state == 0){
   var steam_logoff_unix = new Date(Number(steam_data["response"]["players"][0]["lastlogoff"]+"000"));
   var steam_logoff = "at " + String(steam_logoff_unix).substring(16,21) + " " + steam_logoff_unix.toDateString();
-  console.log(steam_data["response"]["players"][0]["lastlogoff"])
-  console.log(steam_logoff_unix);
-  console.log(steam_logoff);
 }
 else{
-  console.log("Online");
   if ("gameid" in steam_data["response"]["players"][0]){
     steam_playing = 2;
     var steam_game = steam_data["response"]["players"][0]["gameextrainfo"];
