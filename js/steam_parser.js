b8d2d822b6269dd18d1ced1d2450b55dd8a277ad
Matthew Wang <matthewang.wang@gmail.com>
2016-03-22 23:48:47 -0400

    finishes steam parser

--- js/steam_parser.js
+++ js/steam_parser.js
@@ -4,16 +4,18 @@ var steam_data = JSON.parse(httpGet("php/steam.php?steamid=76561198110942970"));
 var steam_state = steam_data["response"]["players"][0]["personastate"];
 
 var steam_status = steam_states[steam_state];
-var steam_playing = false;
+var steam_playing = 0;
 if (steam_state == 0){
   var steam_logoff = steam_data["response"]["players"][0]["lastlogoff"];
 }
 else{
   console.log("Online");
   if ("gameid" in steam_data["response"]["players"][0]){
-    steam_playing = true;
-    var steam_gameid = steam_data["response"]["players"][0];
-    console.log("Playing " + steam_gameid);
+    steam_playing = 2;
+    var steam_game = steam_data["response"]["players"][0]["gameextrainfo"];
+  }
+  else{
+    steam_playing = 1;
   }
 }
 console.log(steam_data);
