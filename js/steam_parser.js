c74fcc64025b00676079097545f6140a2c5c87e6
Matthew Wang <matthewang.wang@gmail.com>
2016-03-22 21:11:16 -0400

    adds persona state checking

--- js/steam_parser.js
+++ js/steam_parser.js
@@ -1,2 +1,12 @@
 var steam_data = JSON.parse(httpGet("php/steam.php?steamid=76561198110942970"));
+var steam_state = steam_data["response"]["players"][0]["personastate"];
+
+if (steam_state == 0){
+  console.log("Offline");
+}
+else{
+  console.log("Online");
+}
+
 console.log(steam_data);
+console.log(steam_data["response"]["players"][0]["personastate"]);
