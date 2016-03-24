51d5399b78f5eafb4335cd1b69659c91449607d1
Matthew Wang <matthewang.wang@gmail.com>
2016-03-23 20:24:25 -0400

    tries modularising instagram stuff

--- js/instagram_parser.js
+++ js/instagram_parser.js
@@ -1,14 +1,16 @@
-$.ajax({
-    url: "http://whateverorigin.org/get?url=" + encodeURIComponent("http://instagram.com/nonotthatmatthewwang/media"),
-    dataType : "jsonp",
-    cache: false,
-    success: function(instagram_data){
-      instagram_photo = instagram_data["contents"]["items"][0]["images"]["thumbnail"]["url"];
-      instagram_caption = instagram_data["contents"]["items"][0]["caption"]["text"];
-      instagram_location = instagram_data["contents"]["items"][0]["location"]["name"];
-      var instagram_date_unix = new Date(Number(instagram_data["contents"]["items"][0]["created_time"]+"000"));
+function instagramFetch(username){
+  $.ajax({
+      url: "http://whateverorigin.org/get?url=" + encodeURIComponent("http://instagram.com/" + username + "/media"),
+      dataType : "jsonp",
+      cache: false,
+      success: function(instagram_data){
+        instagram_photo = instagram_data["contents"]["items"][0]["images"]["thumbnail"]["url"];
+        instagram_caption = instagram_data["contents"]["items"][0]["caption"]["text"];
+        instagram_location = instagram_data["contents"]["items"][0]["location"]["name"];
+        var instagram_date_unix = new Date(Number(instagram_data["contents"]["items"][0]["created_time"]+"000"));
 
-      instagram_date = String(instagram_date_unix).substring(16,21) + " " + instagram_date_unix.toDateString();
-      instaInfo();
-    }
-});
+        instagram_date = String(instagram_date_unix).substring(16,21) + " " + instagram_date_unix.toDateString();
+        instaInfo();
+      }
+  });
+}
