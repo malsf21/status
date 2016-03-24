f60de880136288948589fdb507926202611577bd
Matthew Wang <matthewang.wang@gmail.com>
2016-03-23 21:06:20 -0400

    forgot to var set this one as well

--- js/twitch_parser.js
+++ js/twitch_parser.js
@@ -1,10 +1,11 @@
 var streaming = false;
+var stream_game = "";
 function twitchFetch(twitchUser){
   var stream_data = JSON.parse(httpGet("https://api.twitch.tv/kraken/streams/"+twitchUser+"?jsoncallback=?"));
 
   if (stream_data["stream"] != null){
     streaming = true;
-    var stream_game = stream_data["stream"]["game"];
+    stream_game = stream_data["stream"]["game"];
   }
   twitchInfo();
 }
