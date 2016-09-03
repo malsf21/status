4b58188cb1b44494d8597e2a41676b1453957469
Matthew Wang <matthewang.wang@gmail.com>
2016-09-02 20:50:32 -0400

    updates request parameter

--- js/structure.js
+++ js/structure.js
@@ -1,6 +1,6 @@
 function httpGet(theUrl){
   var xmlHttp = new XMLHttpRequest();
-  xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+  xmlHttp.open( "GET", theUrl +'?_=' + new Date().getTime(), false);
   xmlHttp.send( null );
   return xmlHttp.responseText;
 }
